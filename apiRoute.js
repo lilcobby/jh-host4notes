466d78e0714339b17527696af368809d40d47548
lilcobby <jshairfield13@gmail.com>
2023-11-07 18:49:31 -0500

    close, pushing empty object to array though

--- apiRoute.js
+++ apiRoute.js
@@ -1,5 +1,22 @@
-const path = "path";
-const fs = "fs";
+const path = require("path");
+const fs = require("fs");
 module.exports = (app) => {
-    
+  app.get("/api/notes", (req, res) => {
+    res.sendFile(path.join(__dirname, "/db/db.json"));
+  });
+
+  app.post("/api/notes", (req, res) => {
+    fs.readFile("./db/db.json", "utf8", (err, data) => {
+      if (err) console.log(err);
+      var note = JSON.parse(data);
+
+      let newNote = { title: req.title, text: req.text };
+
+      note.push(newNote);
+
+      fs.writeFile("./db/db.json", JSON.stringify(note), (err, data) => {
+        res.json(note);
+      });
+    });
+  });
 };
