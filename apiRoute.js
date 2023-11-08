70a25d3d6d6a1929c89d2ae1941b2de61e9150bc
lilcobby <jshairfield13@gmail.com>
2023-11-07 19:31:49 -0500

    got it working, time to make delete button

--- apiRoute.js
+++ apiRoute.js
@@ -1,5 +1,6 @@
 const path = require("path");
 const fs = require("fs");
+const uniqid = require("uniqid");
 module.exports = (app) => {
   app.get("/api/notes", (req, res) => {
     res.sendFile(path.join(__dirname, "/db/db.json"));
@@ -9,8 +10,12 @@ module.exports = (app) => {
     fs.readFile("./db/db.json", "utf8", (err, data) => {
       if (err) console.log(err);
       var note = JSON.parse(data);
-
-      let newNote = { title: req.title, text: req.text };
+      console.log(req.body);
+      let newNote = {
+        title: req.body.title,
+        text: req.body.text,
+        id: uniqid(),
+      };
 
       note.push(newNote);
 
--- server.js
+++ server.js
@@ -3,14 +3,13 @@ const app = express();
 const PORT = 3001;
 // route to html get and api post
 
-require("./htmlRoute")(app);
-require("./apiRoute")(app);
-
 // middleware to route from public file
 // middleware to parse req.body
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static("public"));
+require("./htmlRoute")(app);
+require("./apiRoute")(app);
 
 app.listen(PORT, () => {
   console.log(`listening at localhost${PORT}`);
