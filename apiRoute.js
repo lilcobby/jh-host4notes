b5225d1ed99dfa5e2bba15f9656e3f7856092385
lilcobby <jshairfield13@gmail.com>
2023-11-07 21:20:55 -0500

    delete finally works...

--- apiRoute.js
+++ apiRoute.js
@@ -1,11 +1,12 @@
 const path = require("path");
 const fs = require("fs");
 const uniqid = require("uniqid");
+// exports post and delete
 module.exports = (app) => {
   app.get("/api/notes", (req, res) => {
     res.sendFile(path.join(__dirname, "/db/db.json"));
   });
-
+  // post works, reads file, parses data, pushes req.body.text and title to the array, and adds new id, converts back to string and writes file
   app.post("/api/notes", (req, res) => {
     fs.readFile("./db/db.json", "utf8", (err, data) => {
       if (err) console.log(err);
@@ -21,6 +22,19 @@ module.exports = (app) => {
 
       fs.writeFile("./db/db.json", JSON.stringify(note), (err, data) => {
         res.json(note);
+      });
+    });
+  });
+  //   delete was in the wrong set of braces... for two hours....
+  // Delete works reads json file, parses array in file, filters for every object id that is not the selected object id, rewrites the json file.
+  app.delete("/api/notes/:id", (req, res) => {
+    fs.readFile("./db/db.json", "utf8", (err, data) => {
+      if (err) console.log(err);
+      var note = JSON.parse(data);
+      console.log(data);
+      let deletedNote = note.filter((notes) => notes.id !== req.params.id);
+      fs.writeFile("./db/db.json", JSON.stringify(deletedNote), (err, data) => {
+        res.json(deletedNote);
       });
     });
   });
